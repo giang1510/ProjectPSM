f114988fbc80c8fe3d3889ccb47d81fedf1ab204
giang phi <giang1510@hotmail.de>
2024-02-22 19:09:37 +0100

    Add retrieving data from api server using evironment variables and member service

--- webClient/src/app/core/components/home/home.component.ts
+++ webClient/src/app/core/components/home/home.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MembersService } from '../../services/members.service';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  users: any;
 
+  constructor(private membersService: MembersService){}
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  // TODO this is just a test - Needs to be removed
+  getUsers(){
+    this.membersService.getUsers().subscribe({
+      next: users => {
+        this.users = users;
+        console.log(users);
+      },
+      error: error => console.log(error)
+    });
+  }
 }
---
+++ webClient/src/app/core/services/members.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+  baseUrl = environment.apiUrl;
+  constructor(private http: HttpClient) { }
+
+  // TODO this method should return a defined data type instead of any
+  getUsers(){
+    return this.http.get<any>(this.baseUrl + 'users');
+  }
+}
