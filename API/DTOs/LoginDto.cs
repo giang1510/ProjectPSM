2f99fa5b18cad251e4f3f3c64758a7be06346afb
giang phi <giang1510@hotmail.de>
2024-03-01 15:33:14 +0100

    Add validation to Login input data

--- API/DTOs/LoginDto.cs
+++ API/DTOs/LoginDto.cs
@@ -1,7 +1,14 @@
-﻿namespace API.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
 
 public class LoginDto
 {
+    [Required(AllowEmptyStrings = false)]
+    [DisplayFormat(ConvertEmptyStringToNull = false)]
     public required string Username { get; set; }
+
+    [Required(AllowEmptyStrings = false)]
+    [DisplayFormat(ConvertEmptyStringToNull = false)]
     public required string Password { get; set; }
 }
