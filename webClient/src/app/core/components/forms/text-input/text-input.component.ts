5c98c58c08ca53609eef7c04145819a123570430
giang phi <giang1510@hotmail.de>
2024-03-16 22:36:19 +0100

    Main|webClient|Create reusable text input form

---
+++ webClient/src/app/core/components/forms/text-input/text-input.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, Self } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ControlValueAccessor, FormControl, NgControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-text-input',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './text-input.component.html',
+  styleUrls: ['./text-input.component.scss']
+})
+export class TextInputComponent implements ControlValueAccessor{
+  @Input() label = '';
+  @Input() type = 'text';
+
+  constructor(@Self() public ngControl: NgControl){
+    this.ngControl.valueAccessor = this;
+  }
+
+  writeValue(obj: any): void {}
+  registerOnChange(fn: any): void {}
+  registerOnTouched(fn: any): void {}
+
+  /**
+   * Easy access to control in html-file
+   */
+  get control(): FormControl{
+    return this.ngControl.control as FormControl;
+  }
+}
