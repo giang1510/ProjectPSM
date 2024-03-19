74e57c4f5a3bff461ccee72e3100348e7f572cef
giang phi <giang1510@hotmail.de>
2024-03-19 18:14:45 +0100

    Main|API|Add new entities for database - Product and ProductPhoto

---
+++ API/Entities/Photo.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Entities;
+
+/// <summary>
+/// Represents photo data row in database
+/// </summary>
+public abstract class Photo
+{
+    public int Id { get; set; }
+    public required string Url { get; set; }
+    public string? PublicId { get; set; }
+}
---
+++ API/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace API.Entities;
+
+/// <summary>
+/// Represents product data row in database
+/// </summary>
+public class Product
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public required string Category { get; set; }
+    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    public DateTime LastUpdated { get; set; } = DateTime.UtcNow;
+    public bool IsActive { get; set; }
+    public string? Manufacturer { get; set; }
+
+    public List<ProductPhoto> Photos { get; set; } = new();
+}
