83a33b1c16192034f07dc4f5ef3a478f17d6e443
giang phi <giang1510@hotmail.de>
2024-04-04 18:08:02 +0200

    Main|webClient|Change product card style

--- webClient/src/app/core/components/products/product-card/product-card.component.ts
+++ webClient/src/app/core/components/products/product-card/product-card.component.ts
@@ -1,14 +1,25 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductCard } from 'src/app/core/models/productCard';
+import { faEye, faHeart, faRandom, faShoppingCart, faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss']
 })
 export class ProductCardComponent {
   @Input() productCard: ProductCard | undefined;
+
+  // Fontawsome icons
+  readonly fa = {
+    star: faStar,
+    shoppingCart: faShoppingCart,
+    heart: faHeart,
+    eye: faEye,
+    random: faRandom
+  }
 }
--- webClient/src/app/core/components/products/product-list/product-list.component.ts
+++ webClient/src/app/core/components/products/product-list/product-list.component.ts
@@ -4,16 +4,20 @@ import { Observable } from 'rxjs';
 import { ProductCard } from 'src/app/core/models/productCard';
 import { ProductService } from 'src/app/core/services/product.service';
 import { ProductCardComponent } from '../product-card/product-card.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEye, faHeart, faRandom, faShoppingCart, faStar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule, ProductCardComponent],
+  imports: [CommonModule, ProductCardComponent, FontAwesomeModule],
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit{
   productCards$: Observable<ProductCard[]> | undefined;
+
+  
 
   constructor(private productService: ProductService){}
 
